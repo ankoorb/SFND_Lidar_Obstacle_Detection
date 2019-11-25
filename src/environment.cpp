3113852f2ce20ac3bf4046b1e829a14424e12c80
Ankoor Bhagat <ankoor@ankoors-mbp.lan>
2019-11-24 18:32:12 -0800

    Bounding Box

--- src/environment.cpp
+++ src/environment.cpp
@@ -62,11 +62,12 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     std::vector<Color> colors = {Color(1, 0, 0), Color(0, 1, 0), Color(0, 0, 1)};
     for (pcl::PointCloud<pcl::PointXYZ>::Ptr cluster : cloudClusters){
         std::cout << "cluster size ";
-        pointProcessor->numPoints(cluster);
-        renderPointCloud(viewer, cluster, "obstCloud" + std::to_string(clusterId), colors[clusterId]);
+        Box box = pointProcessor->BoundingBox(cluster);
+        renderBox(viewer, box, clusterId);
+//        pointProcessor->numPoints(cluster);
+//        renderPointCloud(viewer, cluster, "obstCloud" + std::to_string(clusterId), colors[clusterId]);
         clusterId++;
     }
-
 }
 
 
